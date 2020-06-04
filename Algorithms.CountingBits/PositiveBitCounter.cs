084bb8c563c4d3298b9dcdbfa9585b2cca273a06
cggarmendia <claudio@merthin.com>
2020-06-04 20:11:26 +0200

    Initial commit.

---
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="PositiveBitCounter.cs" company="Payvision">
+// Copyright (c) Payvision. All rights reserved.
+// </copyright>
+
+namespace Algorithms.CountingBits
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class PositiveBitCounter
+    {
+        public IEnumerable<int> Count(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
