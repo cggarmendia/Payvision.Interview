7c7eba4c41e2303abcf1771cefa4098c4592bf9c
cggarmendia <claudio@merthin.com>
2020-06-04 22:11:56 +0200

    Add orderDeserializer implementation and contract.

---
+++ Refactoring.FraudDetection/Contracts/IOrderDeserializer.cs
@@ -0,0 +1,10 @@
+﻿using Refactoring.FraudDetection.Dto;
+using System.Collections.Generic;
+
+namespace Refactoring.FraudDetection.Contracts
+{
+    public interface IOrderDeserializer
+    {
+        IEnumerable<Order> Deserialize(IEnumerable<string> orders);
+    }
+}
---
+++ Refactoring.FraudDetection/Implementations/OrderDeserializer.cs
@@ -0,0 +1,32 @@
+﻿using Refactoring.FraudDetection.Contracts;
+using Refactoring.FraudDetection.Dto;
+using System;
+using System.Collections.Generic;
+
+namespace Refactoring.FraudDetection.Implementations
+{
+    public class OrderDeserializer : IOrderDeserializer
+    {
+        #region Public_Methods
+        public IEnumerable<Order> Deserialize(IEnumerable<string> orders)
+        {
+            foreach (var order in orders)
+            {
+                var orderSplited = order.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                yield return new Order
+                {
+                    OrderId = int.Parse(orderSplited[0]),
+                    DealId = int.Parse(orderSplited[1]),
+                    Email = orderSplited[2].ToLower(),
+                    Street = orderSplited[3].ToLower(),
+                    City = orderSplited[4].ToLower(),
+                    State = orderSplited[5].ToLower(),
+                    ZipCode = orderSplited[6],
+                    CreditCard = orderSplited[7]
+                };
+            }
+        }
+        #endregion
+    }
+}
