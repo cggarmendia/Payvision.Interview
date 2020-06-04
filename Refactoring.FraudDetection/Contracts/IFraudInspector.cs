9a6598a84ba9c0f56e52500565f61b0dde0472c1
cggarmendia <claudio@merthin.com>
2020-06-04 22:13:07 +0200

    Add fraudInspector implementation and contract.

---
+++ Refactoring.FraudDetection/Contracts/IFraudInspector.cs
@@ -0,0 +1,10 @@
+﻿using Refactoring.FraudDetection.Dto;
+using System.Collections.Generic;
+
+namespace Refactoring.FraudDetection.Contracts
+{
+    public interface IFraudInspector
+    {
+        IEnumerable<FraudResult> InspectOrders(IList<Order> orders);
+    }
+}
---
+++ Refactoring.FraudDetection/Implementations/FraudInspector.cs
@@ -0,0 +1,58 @@
+﻿using Refactoring.FraudDetection.Contracts;
+using Refactoring.FraudDetection.Dto;
+using System.Collections.Generic;
+
+namespace Refactoring.FraudDetection.Implementations
+{
+    public class FraudInspector : IFraudInspector
+    {
+        #region Public_Methods
+        public IEnumerable<FraudResult> InspectOrders(IList<Order> orders)
+        {
+            var fraudResults = new List<FraudResult>();
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                var currentOrder = orders[i];
+
+                for (int j = i + 1; j < orders.Count; j++)
+                {
+                    var orderToCompare = orders[j];
+
+                    if (currentOrder.DealId == orderToCompare.DealId)
+                    {
+                        if (IsTheSameEmailButDifferentCreditCard(currentOrder, orderToCompare)
+                            || IsTheSameAddressButDifferentCreditCard(currentOrder, orderToCompare))
+                        {
+                            fraudResults.Add(new FraudResult
+                            {
+                                IsFraudulent = true,
+                                OrderId = orderToCompare.OrderId
+                            });
+                        }
+                    }
+                }
+            }
+
+            return fraudResults;
+        }
+        #endregion
+
+        #region Private_Methods
+        private static bool IsTheSameEmailButDifferentCreditCard(Order currentOrder, Order orderToCompare)
+        {
+            return currentOrder.Email == orderToCompare.Email
+                && currentOrder.CreditCard != orderToCompare.CreditCard;
+        }
+
+        private static bool IsTheSameAddressButDifferentCreditCard(Order currentOrder, Order orderToCompare)
+        {
+            return currentOrder.State == orderToCompare.State
+                        && currentOrder.ZipCode == orderToCompare.ZipCode
+                        && currentOrder.Street == orderToCompare.Street
+                        && currentOrder.City == orderToCompare.City
+                        && currentOrder.CreditCard != orderToCompare.CreditCard;
+        }
+        #endregion
+    }
+}
