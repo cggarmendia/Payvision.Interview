72e5d9a7283ff0c920be461174a8ef8ebb82f7b5
cggarmendia <claudio@merthin.com>
2020-06-05 02:22:53 +0200

    Add PositiveBitCounterPerformance and PositiveBitCounterPerformanceTest.

---
+++ Algorithms.CountingBits.Tests/PositiveBitCounterPerformanceTest.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="PositiveBitCounterTest.cs" company="Payvision">
+// Copyright (c) Payvision. All rights reserved.
+// </copyright>
+
+namespace Algorithms.CountingBits.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PositiveBitCounterPerformanceTest
+    {
+        private readonly PositiveBitCounterPerformance bitCounterPerformance = new PositiveBitCounterPerformance();
+
+        [TestMethod]
+        public void The_Best_Count_Method_Is_CountWithLinkedList()
+        {
+            var list = Enumerable.Range(1, 100000);
+
+            var countWithLinkedListTimeSpan = ExecuteBitCounterMethod(list, bitCounterPerformance.CountWithLinkedList);
+            Thread.Sleep(1000);
+            var countWithListTimeSpan = ExecuteBitCounterMethod(list, bitCounterPerformance.CountWithList);
+            Thread.Sleep(1000);
+            var countWithArrayListTimeSpan = ExecuteBitCounterMethod(list, bitCounterPerformance.CountWithArrayList);
+
+            Assert.AreEqual(
+                expected: true,
+                actual: (countWithLinkedListTimeSpan < countWithArrayListTimeSpan) && (countWithLinkedListTimeSpan < countWithListTimeSpan),
+                message: $"countWithListTimeSpan: {countWithListTimeSpan.TotalMilliseconds}, countWithArrayListTimeSpan: {countWithArrayListTimeSpan.TotalMilliseconds},  countWithLinkedListTimeSpan: {countWithLinkedListTimeSpan.TotalMilliseconds}");
+        }
+
+        private TimeSpan ExecuteBitCounterMethod(IEnumerable<int> list, Action<int> method)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (var item in list)
+            {
+                method(item);
+            }
+            var ts = stopWatch.Elapsed;
+            stopWatch.Stop();
+            return ts;
+        }
+    }
+}
---
+++ Algorithms.CountingBits/PositiveBitCounterPerformance.cs
@@ -0,0 +1,112 @@
+﻿// <copyright file="PositiveBitCounter.cs" company="Payvision">
+// Copyright (c) Payvision. All rights reserved.
+// </copyright>
+
+namespace Algorithms.CountingBits
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class PositiveBitCounterPerformance
+    {
+        private const string EXCEPTION_MESSAGE = "The input must be greater than or equal to zero.";
+        private const char POSITVE_BIT = '1';
+
+        #region CountWithArrayList
+        public void CountWithArrayList(int input)
+        {
+            if (input < 0)
+                throw new ArgumentException(EXCEPTION_MESSAGE);
+
+            var result = GetPositivesIndexWithArrayList(Convert.ToString(input, 2), out var positivesCount);
+
+            result[0] = positivesCount;
+        }
+
+        private ArrayList GetPositivesIndexWithArrayList(string binaryString, out int positivesCount)
+        {
+            positivesCount = 0;
+            var positivesIndex = new ArrayList() { 0 };
+            var binaryStringLastIndex = binaryString.Length - 1;
+
+            for (var index = binaryStringLastIndex; index >= 0; index--)
+            {
+                if (binaryString[index].Equals(POSITVE_BIT))
+                {
+                    positivesIndex.Add(Math.Abs(index - binaryStringLastIndex));
+                    positivesCount++;
+                }
+            }
+
+            return positivesIndex;
+        }
+        #endregion
+
+        #region CountWithLinkedList
+        public void CountWithLinkedList(int input)
+        {
+            if (input < 0)
+                throw new ArgumentException(EXCEPTION_MESSAGE);
+
+            var result = GetPositivesIndexWithLinkedList(Convert.ToString(input, 2), out var positivesCount);
+
+            result.AddFirst(positivesCount);
+        }
+
+        private LinkedList<int> GetPositivesIndexWithLinkedList(string binaryString, out int positivesCount)
+        {
+            positivesCount = 0;
+            var positivesIndex = new LinkedList<int>();
+            var binaryStringLastIndex = binaryString.Length - 1;
+
+            for (var index = binaryStringLastIndex; index >= 0; index--)
+            {
+                if (binaryString[index].Equals(POSITVE_BIT))
+                {
+                    positivesIndex.AddLast(Math.Abs(index - binaryStringLastIndex));
+                    positivesCount++;
+                }
+            }
+
+            return positivesIndex;
+        }
+        #endregion
+
+        #region CountWithList
+        public void CountWithList(int input)
+        {
+            if (input < 0)
+                throw new ArgumentException(EXCEPTION_MESSAGE);
+
+
+            var positivesIndex = GetPositivesIndexWithList(Convert.ToString(input, 2), out var positivesCount);
+
+            var result = new List<int>(positivesCount + 1)
+            {
+                positivesCount
+            };
+
+            result.AddRange(positivesIndex);
+        }
+
+        private List<int> GetPositivesIndexWithList(string binaryString, out int positivesCount)
+        {
+            positivesCount = 0;
+            var positivesIndex = new List<int>();
+            var binaryStringLastIndex = binaryString.Length - 1;
+
+            for (var index = binaryStringLastIndex; index >= 0; index--)
+            {
+                if (binaryString[index].Equals(POSITVE_BIT))
+                {
+                    positivesIndex.Add(Math.Abs(index - binaryStringLastIndex));
+                    positivesCount++;
+                }
+            }
+
+            return positivesIndex;
+        }
+        #endregion
+    }
+}
